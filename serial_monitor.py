4dfd7de1b2bc6b7705b10e25205b9099979de79f
xinfeng Chen <60170100+cxf9203@users.noreply.github.com>
2022-04-10 15:01:04 +0800

    Update serial_monitor.py

--- serial_monitor.py
+++ serial_monitor.py
@@ -1,27 +1,18 @@
 import serial
 import time
 
-serialPort = "COM9"  # 串口
+serialPort = "COM3"  # 串口
 baudRate = 9600  # 波特率
 ser = serial.Serial(serialPort, baudRate, timeout=0.5)
 print("参数设置：串口=%s ，波特率=%d" % (serialPort, baudRate))
 
 
 while 1:
-    line = ser.readline().decode()
-    sensorvalue = int(line[9:13])
-    output = int(line[24:27])
-    lightsensor = int(line[41:45])
-    temp = int(line[52:57])
+    if ser.in_waiting:
+        str1 = ser.readline().decode("gbk")
 
-    time.sleep(0.5)
-    print(line)
-    print(sensorvalue)
-    print(output)
-    print(lightsensor)
-    print(temp)
-    print(type(sensorvalue))
+        print(str1)
 
 
 
-ser.close
+
